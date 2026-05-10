3b80a1e57971e1c0f3ae9740eedc1cbc7ed3f79e
Navas . M <navasmhd79@gmail.com>
2026-05-10 13:13:43 +0530

    update app name and icon

---
+++ app.config.js
@@ -0,0 +1,69 @@
+const IS_DEV = process.env.APP_VARIANT === 'development';
+
+export default {
+  expo: {
+    name: IS_DEV ? 'Binge (Dev)' : 'Binge',
+    slug: 'series-tracker',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/images/icon.png',
+    scheme: 'seriestracker',
+    userInterfaceStyle: 'automatic',
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: IS_DEV ? 'com.seriestracker.app.dev' : 'com.seriestracker.app',
+    },
+    android: {
+      package: IS_DEV ? 'com.seriestracker.app.dev' : 'com.seriestracker.app',
+      googleServicesFile: './google-services.json',
+      adaptiveIcon: {
+        backgroundColor: '#E6F4FE',
+        foregroundImage: './assets/images/android-icon-foreground.png',
+        backgroundImage: './assets/images/android-icon-background.png',
+        monochromeImage: './assets/images/android-icon-monochrome.png',
+      },
+      edgeToEdgeEnabled: true,
+      predictiveBackGestureEnabled: false,
+    },
+    web: {
+      output: 'static',
+      favicon: './assets/images/favicon.png',
+    },
+    plugins: [
+      'expo-router',
+      '@react-native-firebase/app',
+      '@react-native-firebase/auth',
+      [
+        'expo-build-properties',
+        {
+          ios: {
+            useFrameworks: 'static',
+          },
+        },
+      ],
+      [
+        'expo-splash-screen',
+        {
+          image: './assets/images/splash-icon.png',
+          imageWidth: 200,
+          resizeMode: 'contain',
+          backgroundColor: '#ffffff',
+          dark: {
+            backgroundColor: '#000000',
+          },
+        },
+      ],
+    ],
+    experiments: {
+      typedRoutes: true,
+      reactCompiler: true,
+    },
+    extra: {
+      router: {},
+      eas: {
+        projectId: 'e6e5f181-df5f-43de-8332-f57b796b7c3e',
+      },
+    },
+  },
+};
