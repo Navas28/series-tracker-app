8dfb3163104b4ce35c22db76d3e8d8ea5035c4d0
Navas . M <navasmhd79@gmail.com>
2026-06-01 10:34:59 +0530

    removed app.json

--- app.config.js
+++ app.config.js
@@ -1,67 +1,70 @@
-const IS_DEV = process.env.APP_VARIANT === 'development';
+const IS_EAS = !!process.env.EAS_BUILD;
 
-export default {
-  expo: {
-    name: IS_DEV ? 'Binge (Dev)' : 'Binge',
-    slug: 'series-tracker',
-    version: '1.0.0',
-    orientation: 'portrait',
-    icon: './assets/images/icon.png',
-    scheme: 'seriestracker',
-    userInterfaceStyle: 'automatic',
-    newArchEnabled: true,
-    ios: {
-      supportsTablet: true,
-      bundleIdentifier: IS_DEV ? 'com.seriestracker.app.dev' : 'com.seriestracker.app',
+/** @type {import('expo/config').ExpoConfig} */
+const config = {
+  name: "Binge",
+  slug: "series-tracker",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/images/icon.png",
+  scheme: "seriestracker",
+  userInterfaceStyle: "automatic",
+  newArchEnabled: true,
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: "com.seriestracker.app",
+  },
+  android: {
+    package: "com.seriestracker.app",
+    googleServicesFile: IS_EAS
+      ? process.env.GOOGLE_SERVICES_JSON
+      : "./google-services.json",
+    adaptiveIcon: {
+      foregroundImage: "./assets/images/adaptive-icon.png",
+      backgroundColor: "#041122",
     },
-    android: {
-      package: IS_DEV ? 'com.seriestracker.app.dev' : 'com.seriestracker.app',
-      googleServicesFile: './google-services.json',
-      adaptiveIcon: {
-        foregroundImage: './assets/images/adaptive-icon.png',
-        backgroundColor: '#041122',
+    edgeToEdgeEnabled: true,
+    predictiveBackGestureEnabled: false,
+  },
+  web: {
+    output: "static",
+    favicon: "./assets/images/favicon.png",
+  },
+  plugins: [
+    "expo-router",
+    "@react-native-firebase/app",
+    "@react-native-firebase/auth",
+    [
+      "expo-build-properties",
+      {
+        ios: {
+          useFrameworks: "static",
+        },
       },
-      edgeToEdgeEnabled: true,
-      predictiveBackGestureEnabled: false,
-    },
-    web: {
-      output: 'static',
-      favicon: './assets/images/favicon.png',
-    },
-    plugins: [
-      'expo-router',
-      '@react-native-firebase/app',
-      '@react-native-firebase/auth',
-      [
-        'expo-build-properties',
-        {
-          ios: {
-            useFrameworks: 'static',
-          },
-        },
-      ],
-      [
-        'expo-splash-screen',
-        {
-          image: './assets/images/splash-icon.png',
-          imageWidth: 200,
-          resizeMode: 'contain',
-          backgroundColor: '#041122',
-          dark: {
-            backgroundColor: '#041122',
-          },
-        },
-      ],
     ],
-    experiments: {
-      typedRoutes: true,
-      reactCompiler: true,
-    },
-    extra: {
-      router: {},
-      eas: {
-        projectId: 'e6e5f181-df5f-43de-8332-f57b796b7c3e',
+    [
+      "expo-splash-screen",
+      {
+        image: "./assets/images/splash-icon.png",
+        imageWidth: 200,
+        resizeMode: "contain",
+        backgroundColor: "#041122",
+        dark: {
+          backgroundColor: "#041122",
+        },
       },
+    ],
+  ],
+  experiments: {
+    typedRoutes: true,
+    reactCompiler: true,
+  },
+  extra: {
+    router: {},
+    eas: {
+      projectId: "e6e5f181-df5f-43de-8332-f57b796b7c3e",
     },
   },
 };
+
+export default config;
